bb45730a85802fe1fde055fe26cd1b48d213f40a
Ansh-jain1122009 <86413791+Ansh-jain1122009@users.noreply.github.com>
2021-09-18 11:40:28 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,27 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+  getCount() { 
+    var playerCountRef = database.ref("playerCount"); 
+    playerCountRef.on("value", data => { 
+      playerCount = data.val(); }); 
+    } //Bp 
+    updateCount(count) { 
+      database.ref("/").update({ 
+        playerCount: count }); }
+ update() { 
+   var playerIndex = "players/player" + this.index; 
+   database.ref(playerIndex).update({ 
+     positionX: this.positionX, positionY: this.positionY, }); 
+    } //Bp 
+    static getPlayersInfo() { 
+      var playerInfoRef = database.ref("players"); 
+      playerInfoRef.on("value", data => { 
+        allPlayers = data.val(); 
+      }); 
+    } 
+  }
